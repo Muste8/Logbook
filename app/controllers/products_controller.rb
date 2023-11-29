19fe3b4ae94ed4fba072c10869f2521a55647784
Muste8 <mustafe.ahassen@gmail.com>
2023-11-29 13:27:03 -0600

    Added bootstrap and header

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,19 @@
 class ProductsController < ApplicationController
     before_action :authenticate_user!
-    
+
     def create
         @customer = Customer.find(params[:customer_id])
         @product = @customer.products.create(product_params)
         redirect_to customer_path(@customer)
+    end
+
+    def destroy
+        @customer = Customer.find(params[:customer_id])
+    
+        @product = @customer.products.find(params[:id])
+    
+        @product.destroy
+        redirect_to customer_path(@customer), notice: 'Product was successfully deleted.'
     end
     
     private
