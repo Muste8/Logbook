96bdee656d5f0cb421d50fd7ed39ee573a7399b7
Muste8 <mustafe.ahassen@gmail.com>
2023-12-04 15:29:24 -0600

    Added Sign in with google and Outh2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
-         validates :first_name, :last_name, :username, presence: true
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :customers
   belongs_to :role
+
+  validates :first_name, :last_name, :username, presence: true
   validates :role, presence: true
+
+  def self.from_omniauth(auth)
+    User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.name.split(" ")[0]
+      user.last_name = auth.info.name.split(" ")[1] # assuming the user model has a name
+      user.username = auth.info.email
+      user.role_id = Role.find_by(name: "Cashier").id
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      user.skip_confirmation!
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_203541) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_172220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_203541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "summary"
+    t.text "description"
+    t.date "start"
+    t.date "end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -60,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_203541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "role_id"
+    t.string "uid"
+    t.string "provider"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
